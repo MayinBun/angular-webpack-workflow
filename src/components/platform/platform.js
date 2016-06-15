46addc13dccc878cda21a1f3d3c062f75962a96c
Mayinbun <bunyamin.gs@gmail.com>
2016-06-15 18:07:41 +0200

    - update deps

--- src/components/platform/platform.js
+++ src/components/platform/platform.js
@@ -3,13 +3,7 @@ import angular from 'angular';
 export default angular.module('mbva.platform', [])
     .service('platform',() => new PlatformService);
 
-const API_ENDPOINT = 'https://origin-api.bva-auctions.com/api/rest';  
-
-class PlatformService {
-    constructor() {
-        this.API_ENDPOINT = API_ENDPOINT;
-    }
-}
+const API_ENDPOINT = 'https://api-acc.bva-auctions.com/api/rest';  
 
 
 class Platform {
